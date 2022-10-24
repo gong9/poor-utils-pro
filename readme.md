0bc4dfe45fca496b868d8554b937cb860fef5d80
gong <1782523482@163.com>
2022-10-24 23:54:21 +0800

    docs: add readme

---
+++ readme.md
@@ -0,0 +1,12 @@
+# poor-utils
+
+## 🚀 Features
+- 🎪 hope includes lodash all api also add some more better ability
+
+## 🦄 Usage
+
+## 📦 Install
+
+## 🧱 Contribute
+
+## 👨‍🚀 Contributors
--- src/filterEmptyVal/index.ts
+++ src/filterEmptyVal/index.ts
@@ -10,7 +10,7 @@ interface TargetObjType {
 const filterEmptyVal = (resJson: TargetObjType) => {
   const obj: any = Array.isArray(resJson) ? [] : {}
   for (const [k, v] of Object.entries(resJson)) {
-    // 处理空值的情况
+    // handle undefined or null
     if (!(v === undefined || v === null))
       obj[k] = typeof v === 'object' ? filterEmptyVal((v as TargetObjType)) : v
   }
