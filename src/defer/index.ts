add85e49baa3acb4b5e8e13166d654206dfd84de
gong <1782523482@163.com>
2022-10-31 09:48:37 +0800

    docs: add defer example

--- src/defer/index.ts
+++ src/defer/index.ts
@@ -5,6 +5,10 @@
  * @param func
  * @param args
  * @returns
+ * @example
+ * const a = 1
+ * defer(() => {})  // The function will not be called first,It can be interpreted as putting into macro task queue
+ * const b = 1
  */
 const defer = function (this: any, func: Function, time = 0, ...args: unknown[]) {
   const isCanUseRef = requestAnimationFrame && typeof requestAnimationFrame === 'function' && (time === 0)
@@ -26,3 +30,4 @@ const defer = function (this: any, func: Function, time = 0, ...args: unknown[])
 }
 
 export default defer
+
