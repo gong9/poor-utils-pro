1173a76899ecc1c843be9707281ef8c2d8d6bc7c
gong <1782523482@163.com>
2022-11-21 10:42:03 +0800

    test: add arrayShuffle test

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import defer from './defer'
 import delay from './delay'
 import flip from './flip'
 import lazyImg from './lazyImg'
-import as from './arrayShuffle'
+import arrayShuffle from './arrayShuffle'
 
 export {
   add,
@@ -31,5 +31,5 @@ export {
   delay,
   flip,
   lazyImg,
-  as,
+  arrayShuffle,
 }
