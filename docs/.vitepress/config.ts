d21c3a758df724d3cc6b963b9501766146e17ed0
gong <517441540@qq.com>
2023-05-29 22:47:41 +0800

    docs: fix publish path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'poor-utils-pro',
   description: 'utils',
-  base: process.env.NODE_ENV === 'production' ? '/resize-observer-vue/' : '/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
