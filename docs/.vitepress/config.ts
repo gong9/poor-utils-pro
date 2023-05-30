f4a4312a80773a795b7c80f55497e875f79efef6
gongzhen <517441540@qq.com>
2023-05-30 13:53:44 +0800

    docs: update docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'github', link: 'https://github.com/gong9/poor-utils' },
     ],
   },
 })
